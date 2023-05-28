bcb0b66e663b1b3330b80aad152bd486c2673f72
Mirra Bagrova <mirrabagrova@gmail.com>
2023-05-28 16:25:03 +0300

    hero

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -4,6 +4,8 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Box from "@mui/material/Box";
+import {styled, TextField} from "@mui/material";
+import classes from "./Faq.module.css";
 
 
 export default function Faq() {
@@ -11,6 +13,39 @@ export default function Faq() {
     const handleChange = (panel) => (event, isExpanded) => {
         setExpanded(isExpanded ? panel : false);
     };
+
+
+    // const FaqAccordion = styled((props) => (
+    //     <Accordion disableGutters elevation={0} square {...props} />
+    // ))(({ theme }) => ({
+    //     border: `1px solid var(--dark-space-50)`,
+    //     // '&:not(:last-child)': {
+    //     //     borderBottom: 0,
+    //     // },
+    //     // '&:before': {
+    //     //     display: 'none',
+    //     // },
+    // }));
+    // const FaqAccordionSummary = styled((props) => (
+    //     <AccordionSummary
+    //
+    //         {...props}
+    //     />
+    // ))(({ theme }) => ({
+    //     backgroundColor:
+    //         theme.palette.mode === 'dark'
+    //             ? 'rgba(255, 255, 255, .05)'
+    //             : 'rgba(0, 0, 0, .03)',
+    //     flexDirection: 'row-reverse',
+    //     '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    //         transform: 'rotate(90deg)',
+    //     },
+    //     '& .MuiAccordionSummary-content': {
+    //         marginLeft: theme.spacing(1),
+    //     },
+    // }));
+
+
 
     return (
         <Box>
@@ -79,13 +114,17 @@ export default function Faq() {
                     id="panel3bh-header"
                 >
                     <h4 sx={{width: '33%', flexShrink: 0}}>
-                        Advanced settings
+                        Какие виды ремонта вы предлагаете?
                     </h4>
                 </AccordionSummary>
                 <AccordionDetails>
                     <Box className={"mon-body-20-reg"}>
-                        Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
-                        amet egestas eros, vitae egestas augue. Duis vel est augue.
+                        <ul>
+                            <li>Предчистовая отделка</li>
+                            <li>Мы сотрудничаем с надежными подрядчиками и предлагаем услуги ремонта под ключ.</li>
+                            <li>Мы можем помочь вам создать дизайн-проект, выбрать материалы, организовать ремонтные работы и обеспечить качественное выполнение проекта.</li>
+                            <li>Ремонт под ключ позволяет вам получить готовый к проживанию дом без лишних хлопот.</li>
+                        </ul>
                     </Box>
                 </AccordionDetails>
             </Accordion>
@@ -97,29 +136,45 @@ export default function Faq() {
                     aria-controls="panel4bh-content"
                     id="panel4bh-header"
                 >
-                    <h4 sx={{width: '33%', flexShrink: 0}}>Personal data</h4>
+                    <h4 sx={{width: '33%', flexShrink: 0}}>
+                        Можете ли вы помочь мне с получением ипотеки для покупки дома?
+                    </h4>
                 </AccordionSummary>
                 <AccordionDetails>
                     <Box className={"mon-body-20-reg"}>
-                        Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
-                        amet egestas eros, vitae egestas augue. Duis vel est augue.
+                        <ul>
+                            <li>Да, мы сотрудничаем с различными банками и кредитными мы сотрудничаем с различными банками и готовы помочь вам с получением ипотеки. </li>
+                            <li>Наши специалисты по ипотечному кредитованию будут работать с вами, чтобы найти наилучшие варианты ипотечных программ, которые соответствуют вашим потребностям и возможностям. </li>
+                            <li>Они проведут с вами консультацию, помогут оформить необходимые документы и сопроводят вас на всех этапах процесса получения ипотеки.</li>
+                        </ul>
                     </Box>
                 </AccordionDetails>
             </Accordion>
 
             {/*5*/}
-            <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+            <Accordion expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
                 <AccordionSummary
                     expandIcon={<ExpandMoreIcon/>}
-                    aria-controls="panel4bh-content"
-                    id="panel4bh-header"
+                    aria-controls="panel5bh-content"
+                    id="panel5bh-header"
                 >
-                    <h4 sx={{width: '33%', flexShrink: 0}}>Personal data</h4>
+                    <h4 sx={{width: '33%', flexShrink: 0}}>
+                        Какие дополнительные услуги вы предоставляете при покупке дома?
+                    </h4>
                 </AccordionSummary>
                 <AccordionDetails>
                     <Box className={"mon-body-20-reg"}>
-                        Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
-                        amet egestas eros, vitae egestas augue. Duis vel est augue.
+                        <ul>
+                            <li>Поиск и покупка дома</li>
+                            <li>Онлайн осмотр объекта, консультация</li>
+                            <li>Оценка недвижимости</li>
+                            <li>Консультация ипотечного брокера</li>
+                            <li>Ведение сделки юридическим отделом компании</li>
+                            <li>Заключение страховых договоров при желании</li>
+                            <li>Помощь в организации ремонтных работ</li>
+                        </ul>
+                        <p>Наша цель - обеспечить вам полное сопровождение и поддержку на каждом этапе покупки дома.
+                        Задавайте любые другие вопросы, связанные с покупкой дома.</p>
                     </Box>
                 </AccordionDetails>
             </Accordion>
